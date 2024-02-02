254cddb158a26a25b1a140f8283a4e67bce43be8
Ayoub SABI <ayoub.sabi@gmail.com>
2024-02-02 03:19:07 +0100

    fix style

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 function Footer(): JSX.Element {
   return (
-    <footer className='mt-auto text-center'>
+    <footer className='footer mt-auto text-center'>
       <p>
-        QR Code generator realised by A.Sabi
+        Made with ❤ by <b>A.Sabi</b>
       </p>
     </footer>
   )
